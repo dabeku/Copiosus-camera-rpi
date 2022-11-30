465112c9c4ff437f19f922460fd540f214de0c89
dabeku <daniel.kuster@gmail.com>
2022-11-30 07:57:30 +0100

    network

---
+++ main.py
@@ -0,0 +1,35 @@
+import signal
+import subprocess
+import os
+import time
+
+import network
+import log
+
+log.debug("Starting...")
+
+def handler(signum, frame):
+    log.debug("STOP IT ---")
+    exit(1)
+
+pid = 0
+
+signal.signal(signal.SIGINT, handler)
+
+network.receive_tcp()
+
+cmd = subprocess.Popen(["ffmpeg", "-loglevel", "quiet", "-f", "avfoundation", "-framerate", "30", "-i", "0", "out.mpg"])
+pid = cmd.pid
+
+log.debug("---")
+log.debug("PID: " + str(pid))
+log.debug("---")
+
+# Mac: ffmpeg -f avfoundation -framerate 30 -i "0" out.mpg
+# ffplay udp://192.168.178.46:8554 -vf "setpts=N/30" -fflags nobuffer -flags low_delay -framedrop
+# ffmpeg -f avfoundation -framerate 30 -i "0" -f mpegtsv  udp://192.168.178.46:8554
+
+while True:
+    time.sleep(0.1)
+
+log.debug("Finished.")
---
+++ network.py
@@ -0,0 +1,7 @@
+import log
+
+#// The port the camera listens to for commands from client
+#define PORT_LISTEN_COMMAND_TCP 6090
+
+def receive_tcp():
+    log.debug("HALLO");
